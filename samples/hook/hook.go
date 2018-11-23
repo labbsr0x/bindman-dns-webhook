bd4ff884f3e44611deeab8ffc12a0b04bc412aa4
Abilio Esteves <eco.abilio@gmail.com>
2018-11-23 14:39:58 -0200

    added TTL to hook sample; update compose

--- samples/hook/hook.go
+++ samples/hook/hook.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/labbsr0x/sandman-dns-webhook/src/hook"
 	"github.com/labbsr0x/sandman-dns-webhook/src/types"
 )
 
 func main() {
-	manager := DummyManager{DNSRecords: make(map[string]types.DNSRecord)}
+	manager := DummyManager{DNSRecords: make(map[string]types.DNSRecord), TTL: 3600}
+
+	// get ttl from env
+	if ttl, err := strconv.Atoi(strings.Trim(os.Getenv("SANDMAN_DNS_TTL"), " ")); err == nil {
+		manager.TTL = ttl
+	}
 	hook.Initialize(&manager)
 }
 
 // DummyManager holds the information for managing a dummy dns server
 type DummyManager struct {
 	DNSRecords map[string]types.DNSRecord
+	TTL        int
 }
 
 // GetDNSRecords retrieves all the dns records being managed
