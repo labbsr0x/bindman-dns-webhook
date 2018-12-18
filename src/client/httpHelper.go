c6a1a367f19ddbf8a6c3cc02a225b7cbf77db804
Werberson Silva <c1258882@bb.com.br>
2018-12-18 15:47:04 -0200

    refactoring and adding support to HTTP PUT method.

--- src/client/httpHelper.go
+++ src/client/httpHelper.go
@@ -11,83 +11,56 @@ import (
 
 // HTTPHelper defines an interface for helper methods that encapsulates http requests complexities
 type HTTPHelper interface {
-	Post(url string, data []byte) (http.Response, []byte, error)
-	Get(url string) (http.Response, []byte, error)
-	Delete(url string) (http.Response, []byte, error)
+	Put(url string, data []byte) (*http.Response, []byte, error)
+	Post(url string, data []byte) (*http.Response, []byte, error)
+	Get(url string) (*http.Response, []byte, error)
+	Delete(url string) (*http.Response, []byte, error)
 }
 
 // BindmanHTTPHelper defines a struct that handles with HTTP requests for a bindman webhook client
 type BindmanHTTPHelper struct{}
 
+// Put wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
+func (bhh *BindmanHTTPHelper) Put(url string, data []byte) (*http.Response, []byte, error) {
+	return bhh.request(url, "PUT", "application/json", data)
+}
+
 // Post wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
-func (bhh *BindmanHTTPHelper) Post(url string, data []byte) (http.Response, []byte, error) {
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	if err != nil {
-		logrus.Errorf("HTTP request creation failed. err=%v", err)
-		return http.Response{}, []byte{}, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	logrus.Debugf("POST request=%v", req)
-	response, err1 := client.Do(req)
-	if err1 != nil {
-		logrus.Errorf("HTTP request invocation failed. err=%v", err1)
-		return http.Response{}, []byte{}, err1
-	}
-
-	logrus.Debugf("Response: %v", response)
-	datar, _ := ioutil.ReadAll(response.Body)
-	logrus.Debugf("Response body: %v", datar)
-	return *response, datar, nil
+func (bhh *BindmanHTTPHelper) Post(url string, data []byte) (*http.Response, []byte, error) {
+	return bhh.request(url, "POST", "application/json", data)
 }
 
 // Get wraps the call to http.NewRequest apis and properly submits a new HTTP GET request
-func (bhh *BindmanHTTPHelper) Get(url string) (http.Response, []byte, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		logrus.Errorf("HTTP request creation failed. err=%s", err)
-		return http.Response{}, []byte{}, err
-	}
-
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	logrus.Debugf("GET request=%v", req)
-	response, err1 := client.Do(req)
-	if err1 != nil {
-		logrus.Errorf("HTTP request invocation failed. err=%s", err1)
-		return http.Response{}, []byte{}, err1
-	}
-
-	logrus.Debugf("Response: %v", response)
-	datar, _ := ioutil.ReadAll(response.Body)
-	logrus.Debugf("Response body: %v", datar)
-	return *response, datar, nil
+func (bhh *BindmanHTTPHelper) Get(url string) (*http.Response, []byte, error) {
+	return bhh.request(url, "GET", "", nil)
 }
 
 // Delete wraps the call to http.NewRequest apis and properly submits a new HTTP DELETE request
-func (bhh *BindmanHTTPHelper) Delete(url string) (http.Response, []byte, error) {
-	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
-		logrus.Errorf("HTTP request creation failed. err=%v", err)
-		return http.Response{}, []byte{}, err
-	}
+func (bhh *BindmanHTTPHelper) Delete(url string) (*http.Response, []byte, error) {
+	return bhh.request(url, "DELETE", "", nil)
+}
 
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	logrus.Debugf("DELETE request=%v", req)
-	response, err1 := client.Do(req)
-	if err1 != nil {
-		logrus.Errorf("HTTP request invocation failed. err=%v", err1)
-		return http.Response{}, []byte{}, err1
-	}
+// request defines a generic method to execute http requests
+func (bhh *BindmanHTTPHelper) request(url, method, contentType string, payload []byte) (httpResponse *http.Response, data []byte, err error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(payload))
+	if err == nil {
+		req.Header.Set("Content-Type", contentType)
 
-	logrus.Debugf("Response: %v", response)
-	datar, _ := ioutil.ReadAll(response.Body)
-	logrus.Debugf("Response body: %v", datar)
-	return *response, datar, nil
+		client := &http.Client{
+			Timeout: time.Second * 10,
+		}
+		logrus.Debugf("%v request=%v", method, req)
+
+		httpResponse, err = client.Do(req)
+		if err == nil {
+			logrus.Debugf("Response: %v", httpResponse)
+			data, _ = ioutil.ReadAll(httpResponse.Body)
+			logrus.Debugf("Response body: %v", data)
+			return
+		}
+		logrus.Errorf("HTTP  %v request invocation failed. err=%v", method, err)
+		return
+	}
+	logrus.Errorf("HTTP %v request creation failed. err=%v", method, err)
+	return
 }
