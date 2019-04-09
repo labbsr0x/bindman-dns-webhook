2dbcc5272cf726245d9df8ba38eba308fa3738d0
C1258882 Werberson Pereira da Silva <c1258882@bb.com.br>
2019-04-09 15:45:21 -0300

    put root error(Err) attribute back in types.Error

--- src/types/error.go
+++ src/types/error.go
@@ -8,29 +8,32 @@ import (
 
 // Error groups together information that defines an error. Should always be used to
 type Error struct {
-	Message string      `json:"message"`
-	Code    int         `json:"code"`
-	Details interface{} `json:"details"`
+	Message string   `json:"message"`
+	Code    int      `json:"code"`
+	Details []string `json:"details"`
+	Err     error    `json:"-"`
 }
 
 // Error() gives a string representing the error; also, forces the Error type to comply with the error interface
 func (e *Error) Error() string {
-	return fmt.Sprintf("ERROR (%d): %s", e.Code, e.Message)
+	msg := fmt.Sprintf("ERROR (%v): %s; \n Inner error: %s", e.Code, e.Message, e.Err)
+	logrus.Debug(msg)
+	return msg
 }
 
 // BadRequestError create an Error instance with http.StatusBadRequest code
-func BadRequestError(message string, details interface{}) *Error {
-	return &Error{message, http.StatusBadRequest, details}
+func BadRequestError(message string, err error, details ...string) *Error {
+	return &Error{Message: message, Err: err, Code: http.StatusBadRequest, Details: details}
 }
 
 // BadRequestError create an Error instance with http.StatusNotFound code
-func NotFoundError(message string, details interface{}) *Error {
-	return &Error{message, http.StatusNotFound, details}
+func NotFoundError(message string, err error, details ...string) *Error {
+	return &Error{Message: message, Err: err, Code: http.StatusNotFound, Details: details}
 }
 
 // BadRequestError create an Error instance with http.StatusInternalServerError code
-func InternalServerError(message string, details interface{}) *Error {
-	return &Error{message, http.StatusInternalServerError, details}
+func InternalServerError(message string, err error, details ...string) *Error {
+	return &Error{Message: message, Err: err, Code: http.StatusInternalServerError, Details: details}
 }
 
 // PanicIfError is just a wrapper to a panic call that propagates error when it's not nil
--- src/types/error_test.go
+++ src/types/error_test.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -11,45 +13,8 @@ func TestError(t *testing.T) {
 
 	msg := err.Error()
 
-	if msg != fmt.Sprintf("ERROR (%d): %s", err.Code, err.Message) {
+	if msg != fmt.Sprintf("ERROR (%v): %s; \n Inner error: %s", err.Code, err.Message, err.Err) {
 		t.Error("Wrong format error message")
-	}
-}
-
-func TestBadRequestError(t *testing.T) {
-	err := BadRequestError("test", nil)
-	if err.Code != http.StatusBadRequest {
-		t.Errorf("Expecting 400 but got %d", err.Code)
-	}
-	if err.Message != "test" {
-		t.Errorf("The error message must the same as passed to constructor function. Expecting message 'test' but got %s", err.Message)
-	}
-	if err.Details != nil {
-		t.Errorf("The error details must the same as passed to constructor function. Expecting 'nil' but got %s", err.Details)
-	}
-}
-func TestNotFoundError(t *testing.T) {
-	err := NotFoundError("test", nil)
-	if err.Code != http.StatusNotFound {
-		t.Errorf("Expecting 404 but got %d", err.Code)
-	}
-	if err.Message != "test" {
-		t.Errorf("The error message must the same as passed to constructor function. Expecting message 'test' but got %s", err.Message)
-	}
-	if err.Details != nil {
-		t.Errorf("The error details must the same as passed to constructor function. Expecting 'nil' but got %s", err.Details)
-	}
-}
-func TestInternalServerError(t *testing.T) {
-	err := InternalServerError("test", nil)
-	if err.Code != http.StatusInternalServerError {
-		t.Errorf("Expecting 500 but got %d", err.Code)
-	}
-	if err.Message != "test" {
-		t.Errorf("The error message must the same as passed to constructor function. Expecting message 'test' but got %s", err.Message)
-	}
-	if err.Details != nil {
-		t.Errorf("The error details must the same as passed to constructor function. Expecting 'nil' but got %s", err.Details)
 	}
 }
 
@@ -84,4 +49,105 @@ func TestPanic(t *testing.T) {
 		}
 	}()
 	Panic(err)
+}
+
+func TestErrorConstructorFunctions(t *testing.T) {
+	type args struct {
+		message string
+		err     error
+		details []string
+	}
+	type want struct {
+		code int
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want        want
+		createError func(message string, err error, details ...string) *Error
+	}{
+		{
+			name: "internal server error",
+			args: args{
+				message: "internal",
+				err:     errors.New("nil"),
+				details: []string{"detail1", "detail2"},
+			},
+			want:        want{code: http.StatusInternalServerError},
+			createError: InternalServerError,
+		},
+		{
+			name: "internal server error with nil err and details",
+			args: args{
+				message: "internal",
+				err:     nil,
+				details: nil,
+			},
+			want:        want{code: http.StatusInternalServerError},
+			createError: InternalServerError,
+		},
+		{
+			name: "bad request",
+			args: args{
+				message: "bad request",
+				err:     errors.New("400"),
+				details: []string{"bad", "request"},
+			},
+			want:        want{code: http.StatusBadRequest},
+			createError: BadRequestError,
+		},
+		{
+			name: "bad request with nil err and details",
+			args: args{
+				message: "bad request",
+				err:     nil,
+				details: nil,
+			},
+			want:        want{code: http.StatusBadRequest},
+			createError: BadRequestError,
+		},
+		{
+			name: "not found",
+			args: args{
+				message: "not found",
+				err:     errors.New("404"),
+				details: []string{"not", "found"},
+			},
+			want:        want{code: http.StatusNotFound},
+			createError: NotFoundError,
+		},
+		{
+			name: "not found with nil err and details",
+			args: args{
+				message: "not found",
+				err:     nil,
+				details: nil,
+			},
+			want:        want{code: http.StatusNotFound},
+			createError: NotFoundError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got *Error
+			if tt.args.details == nil {
+				got = tt.createError(tt.args.message, tt.args.err)
+			} else {
+				got = tt.createError(tt.args.message, tt.args.err, tt.args.details...)
+			}
+
+			if got.Message != tt.args.message {
+				t.Errorf("expect message '%s' but got `%s`", tt.args.message, got.Message)
+			}
+			if !reflect.DeepEqual(got.Details, tt.args.details) {
+				t.Errorf("expect detail '%v' but got `%v`", tt.args.details, got.Details)
+			}
+			if !reflect.DeepEqual(got.Err, tt.args.err) {
+				t.Errorf("expect Err '%#v' but got `%#v`", tt.args.err, got.Err)
+			}
+			if got.Code != tt.want.code {
+				t.Errorf("error code = %v, want %v", got.Code, tt.want.code)
+			}
+		})
+	}
 }
